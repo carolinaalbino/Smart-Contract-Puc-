ce1aa265f5da302f99f4f20a58cb34b3b9eb2ca4
carolinaalbino <92129699+carolinaalbino@users.noreply.github.com>
2021-10-07 21:41:15 -0300

    criação do cartao.sol

---
+++ cartao.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.8.9;
+
+contract CartaoDeVacinacao {
+     // autor: Carolina
+     string constant public nome = "Carolina Albino";
+     string public unidade = "Bom Jesus";
+     uint public CNES = 123;
+     uint public data = 12082021;
+     uint private lote = 123;
+     string constant private fabricante = "Pfizer";
+     string public vacinador = "Amanda";
+     bool public imunizado = false;
+     address private wallet = 0xf9c86a03310B0AD362F8E8222315Ae6475e804b4;
+     
+}
